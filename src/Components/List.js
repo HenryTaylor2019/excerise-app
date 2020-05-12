7df136cb04f092e10116ae8960b1902a35992a49
HenryTaylor2019 <henrytaylor@live.co.uk>
2020-05-12 16:10:37 +0100

    Bootstrap css added

--- src/Components/List.js
+++ src/Components/List.js
@@ -5,19 +5,21 @@ import { ExerciseContext } from '../Contexts/ExerciseContext';
 class List extends React.Component {
     static contextType = ExerciseContext;
 
-    
+
 
     render() {
         const { handleinput, exerciseName } = this.context;
 
         return (
             <>
-            <div>
-                <form>
-                    <label>Exercise</label>
-                    <input value={exerciseName} type="text" onInput={handleinput}/>
-                </form>
-            </div>
+                <div>
+                    <form>
+                        <div className="form-group">
+                            <label>Exercise:</label>
+                            <input value={exerciseName} className="form-control" type="text" onInput={handleinput} />
+                        </div>
+                    </form>
+                </div>
 
             </>
         )
